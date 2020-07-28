4f79316956cfb2e854bc455bc063bd99d225f92f
Saul Forman <saul.forman@emory.edu>
2020-07-28 15:46:50 -0500

    stack penalties first

--- drive_env.py
+++ drive_env.py
@@ -82,16 +82,19 @@ class DriveEnv(gym.Env):
 
 
 		if play_type == "run":
+			touchdown = predict_touchdown(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
 			yards_gained = predict_run_yards(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
 			turnover = predict_run_turnover(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
-			penalty = predict_penalty(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
-			seconds_used = predict_seconds(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
+			penalty = predict_run_penalty(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
+			seconds_used = predict_run_seconds(self.rb_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
 		elif play_type == "pass"
 			# not handling scrambles, don't know what passing route would have been
+			# predict touchdowns first, (yards to endzone acts as cap on yards_gained near endzone)
+			touchdown = predict_touchdown(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, pass_location, pass_length)
 			yards_gained = predict_pass_yards(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, pass_location, pass_length)
-			turnover = predict_run_turnover(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, pass_location, pass_length)
-			penalty = predict_penalty(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
-			seconds_used = predict_seconds(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, pass_location, pass_length)
+			turnover = predict_pass_turnover(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, pass_location, pass_length)
+			penalty = predict_pass_penalty(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, run_location, run_gap)
+			seconds_used = predict_pass_seconds(self.qb_id, self.wr_id, self.def_id, shotgun, no_huddle, qb_dropback, pass_location, pass_length)
 		elif play_type == "field goal":
 			fg_success = predict_fg_success(self.k_id, self.def_id, self.yards_to_endzone)
 		# punt
@@ -99,41 +102,46 @@ class DriveEnv(gym.Env):
 			reward = 0
 			done = True
 
-		# take time off the clock
-		self.half_seconds -= seconds_used
-
-		# if first down, reset downs and increment yards to first down
-		# otherwise, increase down
-		if yards_gained >= self.yards_to_first:
-			self.down = 1
-			self.yards_to_first = np.min(10, self.yards_to_endzone)
+		# for now, just modeling holding probability, back up 5 yards
+		if penalty == 1:
+			yards_to_endzone += 5
+			yards_to_first += 5
 		else:
-			self.down += 1
+			# take time off the clock
+			self.half_seconds -= seconds_used
 
-		# if safety, reward is -2
-		if yards_to_endzone - yards_gained >= 100:
-			done = True
-			reward = -2
+			# if first down, reset downs and increment yards to first down
+			# otherwise, increase down
+			if yards_gained >= self.yards_to_first:
+				self.down = 1
+				self.yards_to_first = np.min(10, self.yards_to_endzone)
+			else:
+				self.down += 1
 
-		# if touchdown, reward is 7
-		if yards_gained >= self.yards_to_endzone:
-			done = True
-			reward = 7
+			# if safety, reward is -2
+			if yards_to_endzone - yards_gained >= 100:
+				done = True
+				reward = -2
 
-		# if field goal, reward is 3
-		if fg_success == 1:
-			done = True
-			reward = 3
+			# if touchdown, reward is 7
+			if touchdown >= 1:
+				done = True
+				reward = 7
 
-		# if turnover on downs, reward is -2
-		if down > 4:
-			done = True
-			reward = -2
+			# if field goal, reward is 3
+			if fg_success == 1:
+				done = True
+				reward = 3
 
-		# if turnover, reward is -2
-		if turnover == 1:
-			done = True
-			reward = -2
+			# if turnover on downs, reward is -2
+			if down > 4:
+				done = True
+				reward = -2
+
+			# if turnover, reward is -2
+			if turnover == 1:
+				done = True
+				reward = -2
 
 		observation = np.array([self.half_seconds, self.yards_to_endzone, self.yards_to_first, self.down]).astype(np.float32)
 
